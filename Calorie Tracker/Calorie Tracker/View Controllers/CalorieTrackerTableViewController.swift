a385341b736948b3c14496646353fe6bf0420fb6
Brandi <blubrandi0@gmail.com>
2020-01-06 00:17:30 -0700

    Saved Context and Action

--- Calorie Tracker/Calorie Tracker/View Controllers/CalorieTrackerTableViewController.swift
+++ Calorie Tracker/Calorie Tracker/View Controllers/CalorieTrackerTableViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class CalorieTrackerTableViewController: UITableViewController {
-    
-    var calories = [String]()
+    var calories = [CalorieCount]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,27 +17,39 @@ class CalorieTrackerTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return calories.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-        
-        cell.textLabel?.text = ""
-        cell.detailTextLabel?.text = ""
+        cell.textLabel?.text = calories[indexPath.row].calories
+        cell.detailTextLabel?.text = "\(calories[indexPath.row].timeStamp)"
+
         return cell
     }
-    
     // MARK: - Methods
 
     @IBAction func addCalorieCountTapped(_ sender: Any) {
-        
+        let alert = UIAlertController(title: "Add Calories", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Total Calories"
+            textField.keyboardType = .numberPad
+        }
+        let action = UIAlertAction(title: "Add", style: .default) { (_) in
+            let totalCalories = alert.textFields?.first?.text
+            let timeStamp = Date.self
+            print(totalCalories)
+            print(timeStamp)
+            let calorieCount = CalorieCount(context: CoreDataStack.context)
+            calorieCount.calories = totalCalories!
+            CoreDataStack.saveContext()
+            self.calories.append(calorieCount)
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
-
--- Calorie Tracker/CoreDataStack.swift
+++ Calorie Tracker/CoreDataStack.swift
@@ -10,16 +10,12 @@ import Foundation
 import CoreData
 
 class CoreDataStack {
-    
     //MARK: - Core Data Stack
     private init() {}
-    
     static var context: NSManagedObjectContext {
         return persistentContainer.viewContext
     }
-    
     static var persistentContainer: NSPersistentContainer = {
-        
         let container = NSPersistentContainer(name: "CalorieTracker" )
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
@@ -28,19 +24,17 @@ class CoreDataStack {
         })
         return container
     }()
-    
     // MARK: - Core Data Saving support
-    
     static func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
+                print("SAVED!!!")
             } catch {
                 let nserror = error as NSError
                 print("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
-    
 }
